b70efd855d3b218fcce4db2ceab3e6449598418a
David Crooks <crodave@gmail.com>
2019-02-15 12:28:42 +0100

    Initial Commit

---
+++ ARRay/ARRay-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ARRay-Bridging-Header.h
+//  ARRay
+//
+//  Created by David Crooks on 15/02/2019.
+//  Copyright © 2019 David Crooks. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
